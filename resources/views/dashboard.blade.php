ae1d2e847ce035eea9f54d382a451b29c3ae8f87
aidilbaihaqi <aidillmusirjun@gmail.com>
2024-05-18 14:08:30 +0700

    edit dashboard: tambahin jumlah data per table

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -43,18 +43,18 @@
         <div class="col-md-6 mb-4 stretch-card transparent">
           <div class="card card-tale">
             <div class="card-body">
-              <p class="mb-4">Today’s Bookings</p>
-              <p class="fs-30 mb-2">4006</p>
-              <p>10.00% (30 days)</p>
+              <h6 class="mb-4">Data Mahasiswa</h6>
+              <p class="fs-30 mb-2">{{ $dm }}</p>
+              <p>Jumlah data mahasiswa terdata</p>
             </div>
           </div>
         </div>
         <div class="col-md-6 mb-4 stretch-card transparent">
           <div class="card card-dark-blue">
             <div class="card-body">
-              <p class="mb-4">Total Bookings</p>
-              <p class="fs-30 mb-2">61344</p>
-              <p>22.00% (30 days)</p>
+              <h6 class="mb-4">Data Kelas</h6>
+              <p class="fs-30 mb-2">{{ $dk }}</p>
+              <p>Jumlah kelas terdata</p>
             </div>
           </div>
         </div>
@@ -63,18 +63,18 @@
         <div class="col-md-6 mb-4 mb-lg-0 stretch-card transparent">
           <div class="card card-light-blue">
             <div class="card-body">
-              <p class="mb-4">Number of Meetings</p>
-              <p class="fs-30 mb-2">34040</p>
-              <p>2.00% (30 days)</p>
+              <h6 class="mb-4">Data Jurusan</h6>
+              <p class="fs-30 mb-2">{{ $dj }}</p>
+              <p>Jumlah jurusan terdata</p>
             </div>
           </div>
         </div>
         <div class="col-md-6 stretch-card transparent">
           <div class="card card-light-danger">
             <div class="card-body">
-              <p class="mb-4">Number of Clients</p>
-              <p class="fs-30 mb-2">47033</p>
-              <p>0.22% (30 days)</p>
+              <h6 class="mb-4">Data Mata Kuliah</h6>
+              <p class="fs-30 mb-2">{{ $dmk }}</p>
+              <p>Jumlah mata kuliah terdata</p>
             </div>
           </div>
         </div>
